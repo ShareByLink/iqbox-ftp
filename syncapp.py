054fd20b3e0af3e68b3b534101839ffe8ef6d405
Sergio Pulgarin <sergio@osop-dev1.(none)>
2013-02-26 15:08:28 -0500

    Implemented checkout functionality

--- syncapp.py
+++ syncapp.py
@@ -7,14 +7,19 @@ dt = datetime.datetime
 
 
 class SyncApp(ftplib.FTP_TLS):
+    """Extending the`ftplib.FTP_TLS` class"""
 
-    def __init__(self, host, localpath):
+    def __init__(self, host, localdir):
         ftplib.FTP_TLS.__init__(self, host)
         
-        self.localpath = localpath
+        self.localdir = localdir
         
-        if not os.path.exists(self.localpath):
-            os.makedirs(self.localpath)
+        if not os.path.exists(self.localdir):
+            os.makedirs(self.localdir)
+    
+    @property
+    def currentdir(self):
+        return self.pwd()
         
     def checkout(self):
         """
@@ -22,7 +27,80 @@ class SyncApp(ftplib.FTP_TLS):
         Any local files will be truncated.
         """
         
+        # Handy variables to keep track of the checkout process.
+        # These lists contain absolute paths only.
+        checked_dirs = list()
+        downloaded = list()
+
+        # Sets '/' as initial directory and initializes `downloading_dir`
+        self.cwd('/')
+        downloading_dir = self.currentdir
+            
+        while True:
+            # Gets the list of sub directories and files inside the 
+            # current directory `downloading_dir`.
+            dir_subdirs = self.getDirs(downloading_dir)
+            dirfiles = self.getFiles(downloading_dir)
+            
+            # Leading '/' in `downloading_dir` breaks the `os.path.join` call
+            localdir = os.path.join(self.localdir, downloading_dir[1:])
+            if not os.path.exists(localdir):
+                # Creates the directory if it doesn't already exists.
+                os.makedirs(localdir)
+
+            for file in dirfiles:
+                # `serverpath` is the absolute path of the file on the server,
+                # download it only if it hasn't been already downloaded
+                serverpath = os.path.join(downloading_dir, file)
+                if serverpath not in downloaded:
+                    # Downloads the file and appends it absolute path to the `downloaded` list
+                    self.downloadFile(serverpath)
+                    downloaded.append(serverpath)
+                    
+            dir_ready = True
+            for dir in dir_subdirs:
+                # `dirpath` is the absolute path of the subdirectory on the server,
+                dirpath = os.path.join(downloading_dir, dir)
+                
+                # `downloading_dir` is ready only when all its subdirectory are on the 
+                # `checked_dirs` list.
+                if dirpath not in checked_dirs:
+                    # Found one subdirectory that is not on `checked_dirs`,
+                    # will process it in the next iteration.
+                    downloading_dir = dirpath
+                    dir_ready = False
+                    
+            if dir_ready is True:
+                # All subdirectories of `downloading_dir` are already in `checked_dirs`
+                if self.currentdir == '/':
+                    # All directories ready and at '/', means checkout is complete
+                    break
+                    
+                else:
+                    # Not at '/'. Current directory is ready so is appended to `checked_dirs`
+                    # Back one directory to find directories that are not in `checked_dirs`
+                    checked_dirs.append(downloading_dir)
+                    downloading_dir = os.path.dirname(downloading_dir)
+                
+    def getFiles(self, path):
+        """
+        This method simply wraps the `nlst` method with an exception handler,
+        and returns an empty list in case an exception is caught.
+        
+        :param path: Relative or absolute path on the server
+       """
+        try:
+            return self.nlst(path)
+        except:
+            return []
+             
     def getDirs(self, path):
+        """
+        Retrieves a list of the directories inside `path`,
+        uses `retrlines` and the LIST command to retrieve the items.
+        
+        :param path: Relative or absolute path on the server
+        """
         dirs = list()
         def handleLine(line):
             """
@@ -44,13 +122,14 @@ class SyncApp(ftplib.FTP_TLS):
         self.retrlines('LIST %s' % path, handleLine)
         return dirs
         
-    def getFile(self, file):
+    def downloadFile(self, file, localpath=None):
         """
         Performs a binary download to the file `file` located on the server.
         `file` parameter can be either absolute or relative, though it can
         fail for relative paths if the current directory is not appropiate.
         
         :param filename: Relative or absolute path to the file
+        :param localpath: Absolute local path where the file will be saved
         """
         
         def handleChunk(chunk):
@@ -65,9 +144,19 @@ class SyncApp(ftplib.FTP_TLS):
             self.downloading.write(chunk)
             self.download_total += len(chunk)
         
-        # Gets the absolute local file path corresponding to the file `file`
-        localname = os.path.join(self.localpath, os.path.basename(file))
-        with open(localname, 'wb') as f:
+        
+        if localpath is None:
+            # Gets the absolute local file path corresponding to the file `file`
+            # removing '/' at the beginnig of `file` so the `os.path.join` call works
+            file_ = file[1:] if file.startswith('/') else file
+            localpath = os.path.join(self.localdir, file_)
+        
+        localdir = os.path.dirname(localpath)
+        if not os.path.exists(localdir):
+            # Creates the directory if it doesn't already exists.
+            os.makedirs(localdir)
+        
+        with open(localpath, 'wb') as f:
             # Opens the file at `localname` which will hold the downloaded file.
             # Object attributes regarding download status are updated accordingly.
             self.downloading = f
@@ -79,7 +168,7 @@ class SyncApp(ftplib.FTP_TLS):
         """
         Uses the MDTM FTP command to find the last modified timestamp
         of the file `filename`.
-        Returns a `datetime.datetime` UTC object representing the file's
+        Returns a `datetime.datetime` object in UTC representing the file's
         last modified date and time.
         
         :param filename: Relative or absolute path to the file
@@ -98,9 +187,10 @@ if __name__ == '__main__':
     app.login('testuser', 'test')
     time = app.lastModified('test2/2.15 MB Download.bin')
     print 'UTC: %s, Local: %s' % (time, time - datetime.timedelta(hours=5))
-    #app.getFile('test2/2.15 MB Download.bin')
     print 'Dirs in "/": %s' % app.getDirs('/')
     print 'Dirs in "/test2": %s' % app.getDirs('/test2')
     print 'Files in "/": %s' % app.nlst('/')
     print 'Files in "/test2": %s' % app.nlst('/test2')
+    
+    app.checkout()
 
