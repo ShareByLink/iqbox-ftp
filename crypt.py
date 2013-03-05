40ca94c4d20845c6dec5cd271043c490e58507ac
Sergio Pulgarin <sergio.pulgarin@gmail.com>
2013-03-04 23:28:34 -0500

    Added crypt.py module

---
+++ crypt.py
@@ -0,0 +1,48 @@
+import os
+import sys
+import base64
+from itertools import izip, cycle
+
+sys.path.append(os.path.abspath(__file__))
+
+# Key should be en 'key' file
+with open('key', 'r') as keyfile:
+    KEY = keyfile.read()
+
+def encrypt(data):
+    """
+    Encrypts the `data` string using and XOR operation and
+    returns the result encoded using base64.
+    
+    :param data: String to be encrypted
+    """
+    
+    encrypted = _do_xor(data)
+
+    return base64.b64encode(encrypted)
+    
+def decrypt(data):
+    """
+    Decrypts the `data` string using and XOR after decoding 
+    it using base64. The `data` string should have been encrypted
+    with the `encrypt` function in this module.
+    
+    :param data: String to be decrypted
+    """
+
+    decrypted = base64.b64decode(data)
+    
+    return _do_xor(decrypted)
+    
+def _do_xor(data):
+    """
+    Performs a XOR operation between `data` and `KEY`
+    strings.
+    Returns the encoded result.
+    
+    :param data: String to be encoded
+    """
+    
+    xored = ''.join(chr(ord(x) ^ ord(y)) for (x,y) in izip(data, cycle(KEY)))
+
+    return xored
